4f970dd08b225824b3b789a16a10c2d2901402ed
takuya <takuya0330_a@yahoo.co.jp>
2024-12-22 17:27:05 +0900

    DirectXMathHelper 追加

---
+++ Sandbox/Source/DirectXMathHelper/Main.cpp
@@ -0,0 +1,13 @@
+﻿#include "DirectXMathHelper.h"
+
+int main(int, char**)
+{
+    using namespace XMH;
+
+    XMFLOAT2 a(0, 0), b(1, 1);
+    auto c = a + b;
+    auto d = a + 1;
+    c += d;
+
+    return 0;
+}
