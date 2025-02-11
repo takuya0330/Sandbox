abaef324d7fe361eb422f1c99736b42b8dc8faee
takuya <takuya0330_a@yahoo.co.jp>
2025-02-11 12:12:11 +0900

    Debug 追加

---
+++ Sandbox/Source/Debug/Assert.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <intrin.h>
+
+#include "Log.h"
+
+#undef _ASSERT
+#undef _ASSERT_MSG
+#undef _ASSERT_RETURN
+
+#if defined(_DEBUG)
+#define _DEBUG_BREAK __debugbreak()
+#else
+#define _DEBUG_BREAK
+#endif
+
+#define VS_OUTPUT_ERROR(...)   Log::Output(__FILE__, "(", __LINE__, "): ERROR: ", __FUNCSIG__, ": ", __VA_ARGS__)
+#define VS_OUTPUT_WARNING(...) Log::Output(__FILE__, "(", __LINE__, "): WARNING: ", __FUNCSIG__, ": ", __VA_ARGS__)
+
+#define _ASSERT(expr)                                     \
+	do                                                    \
+	{                                                     \
+		if (!(expr))                                      \
+		{                                                 \
+			VS_OUTPUT_ERROR("Assertion failed: ", #expr); \
+			_DEBUG_BREAK;                                 \
+		}                                                 \
+	} while (0)
+
+#define _ASSERT_MSG(expr, ...)                                         \
+	do                                                                 \
+	{                                                                  \
+		if (!(expr))                                                   \
+		{                                                              \
+			VS_OUTPUT_ERROR("Assertion failed: ", #expr, __VA_ARGS__); \
+			_DEBUG_BREAK;                                              \
+		}                                                              \
+	} while (0)
+
+#define _ASSERT_RETURN(expr, ...) \
+	do                            \
+	{                             \
+		if (!(expr))              \
+		{                         \
+			_ASSERT(expr);        \
+			return __VA_ARGS__;   \
+		}                         \
+	} while (0)
---
+++ Sandbox/Source/Debug/Main.cpp
@@ -0,0 +1,15 @@
+﻿#include "Assert.h"
+
+int main(int, char**)
+{
+	Log::Outputf("[Test] OutputA: %d\n", 0);
+	Log::Outputf(L"[Test] OutputW: %d\n", 1);
+	Log::Output("[Test] ", "Output: ", 2, "\n");
+
+    Log::Output(__FILE__, "(", __LINE__, "): ERROR: ", "Format Test");
+    Log::Output(__FILE__, "(", __LINE__, "): WARNING: ", "Format Test");
+
+    _ASSERT(0);
+
+	return 0;
+}
