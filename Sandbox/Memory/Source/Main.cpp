00304a3ce7ee892d378ecf9a248c13b73be71ca8
takuya <takuya0330_a@yahoo.co.jp>
2025-05-03 12:07:34 +0900

    Memory: BlockAllocator を別ファイルに移動

--- Sandbox/Memory/Source/Main.cpp
+++ Sandbox/Memory/Source/Main.cpp
@@ -1,146 +1,9 @@
 ﻿#include <iostream>
-#include <source_location>
+
+#include "BlockAllocator.h"
 
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
-
-template<typename T>
-constexpr T Alignment(T size, T align)
-{
-	return (size + (align - 1)) & ~(align - 1);
-}
-
-class BlockAllocator
-{
-public:
-	BlockAllocator()
-	    : m_memory(nullptr)
-	    , m_head(nullptr)
-	    , m_alignment(0)
-	    , m_block_size(0)
-	    , m_block_count(0)
-	    , m_meta_size(0)
-	{
-	}
-
-	bool Initialize(size_t alignment, size_t block_size, size_t block_count)
-	{
-		m_alignment   = alignment;
-		m_block_size  = block_size;
-		m_block_count = block_count;
-
-#if defined(_DEBUG)
-		m_meta_size = Alignment(sizeof(Metadata), m_alignment);
-#endif
-
-		size_t aligned_size = Alignment(m_block_size, m_alignment) + m_meta_size;
-		m_memory            = std::malloc(aligned_size * m_block_count);
-		if (!m_memory)
-			return false;
-
-		auto base = reinterpret_cast<uint8_t*>(m_memory);
-		for (size_t i = 0; i < m_block_count; ++i)
-		{
-#if defined(_DEBUG)
-			auto meta  = reinterpret_cast<Metadata*>(base + i * aligned_size);
-			meta->file = "";
-			meta->line = -1;
-			meta->used = false;
-#endif
-
-			auto node  = reinterpret_cast<Node*>(base + i * aligned_size + m_meta_size);
-			node->next = m_head;
-			m_head     = node;
-		}
-
-		return true;
-	}
-
-	void Finalize()
-	{
-		if (!m_memory)
-			return;
-
-		dump();
-		std::free(m_memory);
-	}
-
-	void* Allocate(
-#if defined(_DEBUG)
-	    const std::source_location& location = std::source_location::current()
-#endif
-	)
-	{
-		if (!m_head)
-			return nullptr;
-
-		auto p = reinterpret_cast<uint8_t*>(m_head);
-
-#if defined(_DEBUG)
-		auto meta  = reinterpret_cast<Metadata*>(p - m_meta_size);
-		meta->used = true;
-		meta->file = location.file_name();
-		meta->line = location.line();
-#endif
-
-		auto node = reinterpret_cast<Node*>(p);
-		m_head    = node->next;
-
-		return node;
-	}
-
-	void Deallocate(void* ptr)
-	{
-#if defined(_DEBUG)
-		auto meta  = reinterpret_cast<Metadata*>(reinterpret_cast<uint8_t*>(ptr) - m_meta_size);
-		meta->used = false;
-#endif
-
-		auto node  = reinterpret_cast<Node*>(reinterpret_cast<uint8_t*>(ptr));
-		node->next = m_head;
-		m_head     = node;
-	}
-
-private:
-	struct Node
-	{
-		Node* next;
-	};
-
-#if defined(_DEBUG)
-	struct Metadata
-	{
-		const char* file;
-		int         line;
-		bool        used;
-	};
-#endif
-
-private:
-	void dump()
-	{
-#if defined(_DEBUG)
-		const auto base         = reinterpret_cast<const uint8_t*>(m_memory);
-		size_t     aligned_size = Alignment(m_block_size, m_alignment) + m_meta_size;
-		for (size_t i = 0; i < m_block_count; ++i)
-		{
-			const auto meta = reinterpret_cast<const Metadata*>(base + i * aligned_size);
-			if (meta->used)
-			{
-				std::cout << "[WARNING] " << meta->file << "(" << meta->line << "): Leaked!" << std::endl;
-			}
-		}
-#endif
-	}
-
-private:
-	void*  m_memory;
-	Node*  m_head;
-	size_t m_alignment;
-	size_t m_block_size;
-	size_t m_block_count;
-	size_t m_meta_size;
-};
 
 int main(int, char**)
 {
@@ -173,10 +36,10 @@ int main(int, char**)
 	std::cout << "address = " << p[3] << ", value = " << *p[3] << std::endl;
 	std::cout << "address = " << p[4] << ", value = " << *p[4] << std::endl;
 
-	//block.Deallocate(p[0]);
+	// block.Deallocate(p[0]);
 	block.Deallocate(p[1]);
 	block.Deallocate(p[2]);
-	//block.Deallocate(p[3]);
+	// block.Deallocate(p[3]);
 	block.Deallocate(p[4]);
 
 	block.Finalize();
