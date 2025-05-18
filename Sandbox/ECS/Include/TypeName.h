3e2e05dd62b6d07573b5fea80ddbec40a6eb8616
takuya <takuya0330_a@yahoo.co.jp>
2025-05-18 11:42:29 +0900

    ECS: TypeNameOf の std::remove_cvref_t 漏れを修正

--- Sandbox/ECS/Include/TypeName.h
+++ Sandbox/ECS/Include/TypeName.h
@@ -2,13 +2,15 @@
 
 #include "TypeNameInternal.h"
 
+#include <type_traits>
+
 namespace ECS {
 
 //! \brief 型名を取得する
 template<typename T>
 constexpr const char* TypeNameOf() noexcept
 {
-	return Internal::name_of_impl<T>().data();
+	return Internal::name_of_impl<std::remove_cvref_t<T>>().data();
 }
 
 } // namespace ECS
