edc807c7789a398bcb191e38a3676d721236fb96
takuya <takuya0330_a@yahoo.co.jp>
2025-05-18 11:37:31 +0900

    ECS: noexcept 付与

--- Sandbox/ECS/Include/TypeName.h
+++ Sandbox/ECS/Include/TypeName.h
@@ -6,7 +6,7 @@ namespace ECS {
 
 //! \brief 型名を取得する
 template<typename T>
-constexpr const char* TypeNameOf()
+constexpr const char* TypeNameOf() noexcept
 {
 	return Internal::name_of_impl<T>().data();
 }
