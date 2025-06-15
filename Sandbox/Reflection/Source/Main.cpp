8672c8edc13ac5124a7c0faf7d15fba641942b4e
takuya <takuya0330_a@yahoo.co.jp>
2025-06-15 10:43:12 +0900

    Reflection: 簡易的なリフレクションを実装

---
+++ Sandbox/Reflection/Source/Main.cpp
@@ -0,0 +1,61 @@
+﻿#include "Reflection.h"
+
+#include <iostream>
+
+struct A
+{
+	int   i;
+	float f;
+	char  c;
+
+	REFLECTION(
+	    A,
+	    REFLECT_FIELD(i),
+	    REFLECT_FIELD(f),
+	    REFLECT_FIELD(c));
+};
+
+struct B
+{
+	double      d;
+	std::string s;
+
+	REFLECTION(
+	    B,
+	    REFLECT_FIELD(d),
+	    REFLECT_FIELD(s));
+};
+
+struct C
+{
+	A a { 42, 3.14f, 'x' };
+	B b { 0.001, "reflection test" };
+
+	REFLECTION(
+	    C,
+	    REFLECT_FIELD(a),
+	    REFLECT_FIELD(b));
+};
+
+struct D
+{
+	C c0 {};
+	C c1 { A { 1, 2.0f, 'a' }, B { 3.14, "hello" } };
+
+    REFLECTION(
+	    D,
+	    REFLECT_FIELD(c0),
+	    REFLECT_FIELD(c1));
+};
+
+int main(int, char**)
+{
+	D d {};
+
+    Reflect(d, [](auto name, auto& field)
+        {
+		    std::cout << "name: " << name << ", field: " << field << std::endl;
+	    });
+
+	return 0;
+}
---
+++ Sandbox/Reflection/Source/Reflection.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include <tuple>
+
+template<typename T>
+concept Reflectable = requires {
+	T::Fields();
+};
+
+template<typename T>
+struct Reflection
+{
+	static constexpr auto Fields()
+	{
+		return T::Fields();
+	}
+
+	template<size_t N>
+	static constexpr auto Field()
+	{
+		return std::get<N>(Fields());
+	}
+
+	template<typename Function>
+	static void Reflect(T& obj, Function&& func)
+	{
+		std::apply([&](auto&&... pair)
+		    {
+			    (func(pair.first, obj.*(pair.second)), ...);
+		    },
+		    Fields());
+	}
+};
+
+template<typename T, typename Function>
+static void Reflect(T& obj, Function&& func)
+{
+	Reflection<T>::Reflect(obj, [&](auto name, auto& field)
+	    {
+		    if constexpr (Reflectable<std::decay_t<decltype(field)>>)
+		    {
+			    Reflect(field, std::forward<Function>(func));
+		    }
+		    else
+		    {
+			    func(name, field);
+		    }
+	    });
+}
+
+#define REFLECTION(Type, ...)                \
+	using This = Type;                       \
+	friend struct Reflection<This>;          \
+	static constexpr auto Fields()           \
+	{                                        \
+		return std::make_tuple(__VA_ARGS__); \
+	}
+
+#define REFLECT_FIELD(Member) \
+	REFLECT_FIELD_IMPL(This, Member)
+
+#define REFLECT_FIELD_WITH_NAME(Name, Member) \
+	REFLECT_FIELD_WITH_NAME_IMPL(Name, This, Member)
+
+#define REFLECT_FIELD_IMPL(Type, Member) \
+	std::make_pair(#Member, &Type::Member)
+
+#define REFLECT_FIELD_WITH_NAME_IMPL(Name, Type, Member) \
+	std::make_pair(#Name, &Type::Member)
