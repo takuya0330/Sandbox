557485ab6cab9ffdcc53664fbeb897be7cd6275d
takuya <takuya0330_a@yahoo.co.jp>
2026-02-23 09:46:10 +0900

    Allocator 追加

---
+++ Sandbox/Allocator/Include/Allocator.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <memory>
+
+class LinearAllocator
+{
+public:
+	LinearAllocator(size_t capacity);
+	~LinearAllocator();
+	void* Allocate(size_t size, size_t alignment);
+	void  Deallocate(void* ptr);
+	void  Reset();
+
+private:
+	std::unique_ptr<uint8_t[]> m_buffer;
+	size_t                     m_capacity;
+	size_t                     m_offset;
+};
---
+++ Sandbox/Allocator/Source/Allocator.cpp
@@ -0,0 +1,47 @@
+﻿#include "../Include/Allocator.h"
+
+namespace {
+
+template<typename T>
+T alignup(T value, size_t alignment)
+{
+	return (value + alignment - 1) & ~(alignment - 1);
+}
+
+} // namespace
+
+LinearAllocator::LinearAllocator(size_t capacity)
+    : m_buffer(new uint8_t[capacity])
+    , m_capacity(capacity)
+    , m_offset(0)
+{
+}
+
+LinearAllocator::~LinearAllocator()
+{
+}
+
+void* LinearAllocator::Allocate(size_t size, size_t alignment)
+{
+	size_t current = reinterpret_cast<size_t>(m_buffer.get() + m_offset);
+	size_t aligned = alignup(current, alignment);
+
+    size_t new_offset = aligned - reinterpret_cast<size_t>(m_buffer.get()) + size;
+
+	if (new_offset > m_capacity)
+		return nullptr;
+
+	m_offset = new_offset;
+
+	return reinterpret_cast<void*>(aligned);
+}
+
+void LinearAllocator::Deallocate(void* ptr)
+{
+	(void)ptr;
+}
+
+void LinearAllocator::Reset()
+{
+	m_offset = 0;
+}
